41463adf7f68617ee80e73d2367d2de92eb14f7d
balez4464 <71140813+balez4464@users.noreply.github.com>
2020-10-02 12:06:18 -0400

    Update Main.java
    had to manually commit

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program tells the user thir hailstone sequence
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // creates a Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user for a positive integer
+    System.out.println("Please enter a positive integer to see the hailstone sequence.");
+    //allows user to put in a positvie number
+    int userNumber = input.nextInt();
+    //count for the sequence
+    int hailstoneCount = 0;
+
+    //while loop with if statments
+    System.out.println("Hailstone sequence is the following:");
+    while(userNumber != 1){
+      //if number matches 1 or not
+      if(userNumber %2 == 0){
+        userNumber = userNumber / 2;
+        System.out.println(userNumber);
+      } else {
+        userNumber = userNumber * 3 + 1;
+        System.out.println(userNumber);
+      } 
+      
+    }
     
   }
 }
